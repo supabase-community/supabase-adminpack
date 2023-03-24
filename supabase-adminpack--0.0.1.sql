cee2b75be940f1339be0cff458b9361d9bc4f017
Michel Pelletier <michel@supabase.io>
2023-03-23 19:44:38 -0700

    add database and schema sizes, last vacuum analyze and table row estimates

--- supabase-adminpack--0.0.1.sql
+++ supabase-adminpack--0.0.1.sql
@@ -394,3 +394,42 @@ FROM
 WHERE
     t.schemaname NOT IN ('pg_catalog', 'information_schema')
 ORDER BY 1, 2;
+
+-- From: https://blog.devgenius.io/top-useful-sql-queries-for-postgresql-35ff3355d265
+
+CREATE VIEW database_sizes AS SELECT pg_database.datname,
+       pg_size_pretty(pg_database_size(pg_database.datname)) AS size
+FROM pg_database
+ORDER BY pg_database_size(pg_database.datname) DESC;
+
+CREATE VIEW schema_sizes SELECT A.schemaname,
+       pg_size_pretty (SUM(pg_relation_size(C.oid))) as table,
+       pg_size_pretty (SUM(pg_total_relation_size(C.oid)-pg_relation_size(C.oid))) as index,
+       pg_size_pretty (SUM(pg_total_relation_size(C.oid))) as table_index,
+       SUM(n_live_tup)
+FROM pg_class C
+LEFT JOIN pg_namespace N ON (N.oid = C .relnamespace)
+INNER JOIN pg_stat_user_tables A ON C.relname = A.relname
+WHERE nspname NOT IN ('pg_catalog', 'information_schema')
+AND C .relkind <> 'i'
+AND nspname !~ '^pg_toast'
+GROUP BY A.schemaname;
+
+CREATE VIEW last_vacuum_analyze AS SELECT relname,
+       last_vacuum,
+       last_autovacuum
+       n_mod_since_analyze,
+       last_analyze,
+       last_autoanalyze,
+       analyze_count,
+       autoanalyze_count
+    FROM pg_stat_user_tables;
+
+CREATE VIEW table_row_estimates AS SELECT
+  schemaname,
+  relname,
+  n_live_tup
+FROM
+  pg_stat_user_tables
+ORDER BY
+  n_live_tup DESC;
